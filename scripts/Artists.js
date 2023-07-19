38c95eb6a4ed2488edccf1a2827fadd7b71d285b
ShayExD <104307803+ShayExD@users.noreply.github.com>
2023-07-19 15:43:15 +0300

    ss

--- scripts/Artists.js
+++ scripts/Artists.js
@@ -1,3 +1,6 @@
+const apiKey = '35a874403974c410da6243eed29981c4';
+
+
 function initArtist(){
         renderHeader();
         renderRightHeader();
@@ -9,9 +12,33 @@ function initArtist(){
 
 function successrenderAllartists(ListOfArtists){
 console.log(ListOfArtists);
+for (let name of ListOfArtists){
+    let artist = name;
+    $.ajax({
+        url: 'http://ws.audioscrobbler.com/2.0/',
+        method: 'GET',
+        data: {
+          method: 'artist.getinfo',
+          artist: artist,
+          api_key: apiKey,
+          format: 'json'
+        },
+        success: function(response) {
+          // Log the response data to the console
+          console.log(response);
+        },
+        error: function(error) {
+          // Handle the error
+          console.error(error);
+        }
+      });
+}
+
+
 }
 
 function errorrenderAllartists(err){
     console.log(err);
     }
 
+  
