9b93c9c68e1e33d4eec1b34750a9d54c70e6a1a2
ShayExD <104307803+ShayExD@users.noreply.github.com>
2023-07-23 20:01:12 +0300

    ss

--- scripts/Favorites.js
+++ scripts/Favorites.js
@@ -14,58 +14,7 @@ function renderFavoriteSongs() {
 
 function successrenderFavoriteSongs(data) {
     console.log(data);
-    const cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = "";
-
-    for (let i = 0; i < data.length; i++)
-    {
-
-        const card = document.createElement('div');
-        card.classList.add('card');
-
-        const nameElement = document.createElement('h3');
-        nameElement.textContent = data[i].songName;
-        card.appendChild(nameElement);
-
-        const idElement = document.createElement('p');
-        idElement.textContent = `Song ID: ${data[i].id}`;
-        card.appendChild(idElement);
-
-        const artistElement = document.createElement('p');
-        artistElement.textContent = `Artist: ${data[i].artist}`;
-        card.appendChild(artistElement);
-
-
-
-        const lyricsBox = document.createElement('button');
-        lyricsBox.textContent = "click for lyrics";
-        lyricsBox.classList.add('AllButtons');
-        lyricsBox.onclick = function () {
-            window.open("lyrics.html", "_blank");
-            localStorage.setItem("lyrics", JSON.stringify(data[i]));
-        }
-
-        card.appendChild(lyricsBox);
-
-        let check = JSON.parse(localStorage.getItem('logged user'))
-        console.log(check)
-
-        if (check != null) {
-            if (check.email != "") {
-                const Favbtn = document.createElement('button');
-                Favbtn.textContent = "Delete From Favorite";
-                Favbtn.classList.add('AllButtons');
-                Favbtn.onclick = function () {
-                    DeleteFromFavorite(data[i].id)
-                }
-                card.appendChild(Favbtn);
-            }
-        }
-
-        cardContainer.appendChild(card);
-    }
-
-
+    renderCards(data,true);
 
 }
 
--- scripts/Library.js
+++ scripts/Library.js
@@ -1,5 +1,7 @@
 
 
+/////////-------------General Functions--------------////////////
+
 function init() { 
     renderHeader()
     renderRightHeader();
@@ -29,87 +31,7 @@ function search() {
 }
 
 
-function renderSearchByArtist()
-{
-    let searchBar = document.getElementById("searchBar");
-    let inputInSearchBar = searchBar.value;
-    const api = `https://localhost:7087/api/Song/getSongsByArtist?artist=${inputInSearchBar}`
-    ajaxCall("GET", api, "", successrenderSearchByArtist, errorrenderSearchByArtist);
-    return false;
-}
-
-function successrenderSearchByArtist(data) {
-    console.log(data);
-    const cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = "";
-
-    for (let i = 0; i < data.length; i++){
-
-        const card = document.createElement('div');
-        card.classList.add('card');
-    
-        const nameElement = document.createElement('h3');
-        nameElement.textContent = data[i].songName;
-        card.appendChild(nameElement);
-    
-        const idElement = document.createElement('p');
-        idElement.textContent = `Song ID: ${data[i].id}`;
-        card.appendChild(idElement);
-    
-        const artistElement = document.createElement('p');
-        artistElement.textContent = `Artist: ${data[i].artist}`;
-        card.appendChild(artistElement);
-    
-    
-
-        const lyricsBox = document.createElement('button');
-        lyricsBox.textContent = "click for lyrics";
-        lyricsBox.classList.add('AllButtons');
-        lyricsBox.onclick = function() {
-             window.open("lyrics.html","_blank");
-             localStorage.setItem("lyrics", JSON.stringify(data[i]));
-        }
-
-        card.appendChild(lyricsBox);
-
-        let check = JSON.parse(localStorage.getItem('logged user'))
-        console.log(check)
-
-        if (check != null) {
-            if (check.email != "") {
-                const Favbtn = document.createElement('button');
-                Favbtn.textContent = "Add To Favorite";
-                Favbtn.classList.add('AllButtons');
-                Favbtn.onclick = function () {
-                    AddToFavorite(data[i].id)
-                }
-                card.appendChild(Favbtn);
-            }
-        }
-
-        cardContainer.appendChild(card);
-    }
-
-
-    
-}
-
-function errorrenderSearchByArtist(err) {
-    const cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = "";
-    swal("Something wrong", "try again", "error");
-}
-
-function renderSearchBySongsName() {
-    let searchBar = document.getElementById("searchBar");
-    let inputInSearchBar = searchBar.value;
-    const api = `https://localhost:7087/api/Song/getSongsBySongName?songName=${inputInSearchBar}`
-    ajaxCall("GET", api, "", successrenderSearchBySongsName, errorrrenderSearchBySongsName);
-    return false;
-}
-
-function successrenderSearchBySongsName(data) {
-    console.log(data);
+function renderCards(data,flag){
     const cardContainer = document.getElementById('cardContainer');
     cardContainer.innerHTML = "";
 
@@ -117,78 +39,7 @@ function successrenderSearchBySongsName(data) {
 
         const card = document.createElement('div');
         card.classList.add('card');
-
-        const nameElement = document.createElement('h3');
-        nameElement.textContent = data[i].songName;
-        card.appendChild(nameElement);
-
-        const idElement = document.createElement('p');
-        idElement.textContent = `Song ID: ${data[i].id}`;
-        card.appendChild(idElement);
-
-        const artistElement = document.createElement('p');
-        artistElement.textContent = `Artist: ${data[i].artist}`;
-        card.appendChild(artistElement);
-
-
-
-        const lyricsBox = document.createElement('button');
-        lyricsBox.textContent = "click for lyrics";
-        lyricsBox.classList.add('AllButtons');
-        lyricsBox.onclick = function () {
-            window.open("lyrics.html", "_blank");
-            localStorage.setItem("lyrics", JSON.stringify(data[i]));
-        }
-
-        card.appendChild(lyricsBox);
-
-        let check = JSON.parse(localStorage.getItem('logged user'))
-        console.log(check)
-
-        if (check != null) {
-            if (check.email != "") {
-                const Favbtn = document.createElement('button');
-                Favbtn.textContent = "Add To Favorite";
-                Favbtn.classList.add('AllButtons');
-                Favbtn.onclick = function () {
-                    AddToFavorite(data[i].id)
-                }
-                card.appendChild(Favbtn);
-            }
-        }
-
-
-        cardContainer.appendChild(card);
-    }
-
-
-
-}
-
-function errorrrenderSearchBySongsName(err) {
-    const cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = "";
-    swal("Something wrong", "try again", "error");
-}
-
-
-function renderSearchAllSongs() {
-    let searchBar = document.getElementById("searchBar");
-    let inputInSearchBar = searchBar.value;
-    const api = `https://localhost:7087/api/Song/GetAllSongs`
-    ajaxCall("GET", api, "", successrenderSearchAllSongs, errorrenderSearchAllSongs);
-    return false;
-}
-
-function successrenderSearchAllSongs(data) {
-    console.log(data);
-    const cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = "";
-
-    for (let i = 0; i < data.length; i++) {
-
-        const card = document.createElement('div');
-        card.classList.add('card');
+        card.id='Id'+data[i].songName;
 
         const nameElement = document.createElement('h3');
         nameElement.textContent = data[i].songName;
@@ -212,6 +63,7 @@ function successrenderSearchAllSongs(data) {
 
         const lyricsBox = document.createElement('button');
         lyricsBox.textContent = "click for lyrics";
+        lyricsBox.id='lyricButton';
         lyricsBox.classList.add('AllButtons');
         lyricsBox.onclick = function () {
             window.open("lyrics.html", "_blank");
@@ -219,34 +71,115 @@ function successrenderSearchAllSongs(data) {
         }
         card.appendChild(lyricsBox);
 
-        const youTubeBox = document.createElement('button');
-        youTubeBox.textContent = "click for YouTube";
+
+        const youTubeBox = document.createElement("button");
         youTubeBox.classList.add('AllButtons');
+        youTubeBox.id='YouTubeButton';
+        const icon = document.createElement("i");
+        icon.className = "fab fa-youtube";
         youTubeBox.onclick = function () {
             renderSendToYouTube(data[i].songName, data[i].artist)
         }
+        const youTubeButtonText = document.createTextNode("YouTube");
+        youTubeBox.appendChild(icon);
+        youTubeBox.appendChild(youTubeButtonText);
         card.appendChild(youTubeBox);
 
+        let UserFromLocal = JSON.parse(localStorage.getItem('logged user'))
 
-        let check = JSON.parse(localStorage.getItem('logged user'))
-
-        if (check != null) {
-            if (check.email != "") { 
+        if (UserFromLocal != null) {
+            if (UserFromLocal.email != "") { 
+                if(!flag){
             const Favbtn = document.createElement('button');
             Favbtn.textContent = "Add To Favorite";
+            Favbtn.id='FavoriteButton';
             Favbtn.classList.add('AllButtons');
             Favbtn.onclick = function () {
                 AddToFavorite(data[i].id)
             }
                 card.appendChild(Favbtn);
             }
+            else{
+                const Favbtn = document.createElement('button');
+                Favbtn.textContent = "Delete From Favorite";
+                Favbtn.id='FavoriteButton';
+                Favbtn.classList.add('AllButtons');
+                Favbtn.onclick = function () {
+                    DeleteFromFavorite(data[i].id);
+                }
+                    card.appendChild(Favbtn);
+
+            }
+        }
         }
 
         cardContainer.appendChild(card);
     }
+}
+
+
+/////////-------------SearchByArtist Functions--------------////////////
+
+function renderSearchByArtist()
+{
+    let searchBar = document.getElementById("searchBar");
+    let inputInSearchBar = searchBar.value;
+    const api = `https://localhost:7087/api/Song/getSongsByArtist?artist=${inputInSearchBar}`
+    ajaxCall("GET", api, "", successrenderSearchByArtist, errorrenderSearchByArtist);
+    return false;
+}
+
+function successrenderSearchByArtist(data) {
+    renderCards(data);
+}
+
+function errorrenderSearchByArtist(err) {
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML = "";
+    swal("Something wrong", "try again", "error");
+}
 
 
 
+/////////-------------SearchBySongsName Functions--------------////////////
+
+
+
+function renderSearchBySongsName() {
+    let searchBar = document.getElementById("searchBar");
+    let inputInSearchBar = searchBar.value;
+    const api = `https://localhost:7087/api/Song/getSongsBySongName?songName=${inputInSearchBar}`
+    ajaxCall("GET", api, "", successrenderSearchBySongsName, errorrrenderSearchBySongsName);
+    return false;
+}
+
+function successrenderSearchBySongsName(data) {
+    renderCards(data,false);
+
+}
+
+function errorrrenderSearchBySongsName(err) {
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML = "";
+    swal("Something wrong", "try again", "error");
+}
+
+
+
+/////////-------------SearchAllSongs Functions--------------////////////
+
+
+
+function renderSearchAllSongs() {
+    let searchBar = document.getElementById("searchBar");
+    let inputInSearchBar = searchBar.value;
+    const api = `https://localhost:7087/api/Song/GetAllSongs`
+    ajaxCall("GET", api, "", successrenderSearchAllSongs, errorrenderSearchAllSongs);
+    return false;
+}
+
+function successrenderSearchAllSongs(data) {
+    renderCards(data,false);
 }
 
 function errorrenderSearchAllSongs(err) {
@@ -256,69 +189,21 @@ function errorrenderSearchAllSongs(err) {
 }
 
 
+
+/////////-------------SearchByLyrics Functions--------------////////////
+
+
+
 function renderSearchByLyrics() {
     let searchBar = document.getElementById("searchBar");
     let inputInSearchBar = searchBar.value;
-    alert(inputInSearchBar);
     const api = `https://localhost:7087/api/Song/getSongsByLyrics?lyrics=${inputInSearchBar}`
     ajaxCall("GET", api, "", successrenderSearchByLyrics, errorrrenderSearchByLyrics);
     return false;
 }
 
 function successrenderSearchByLyrics(data) {
-    console.log(data);
-    const cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = "";
-
-    for (let i = 0; i < data.length; i++) {
-
-        const card = document.createElement('div');
-        card.classList.add('card');
-
-        const nameElement = document.createElement('h3');
-        nameElement.textContent = data[i].songName;
-        card.appendChild(nameElement);
-
-        const idElement = document.createElement('p');
-        idElement.textContent = `Song ID: ${data[i].id}`;
-        card.appendChild(idElement);
-
-        const artistElement = document.createElement('p');
-        artistElement.textContent = `Artist: ${data[i].artist}`;
-        card.appendChild(artistElement);
-
-
-
-        const lyricsBox = document.createElement('button');
-        lyricsBox.textContent = "click for lyrics";
-        lyricsBox.classList.add('AllButtons');
-        lyricsBox.onclick = function () {
-            window.open("lyrics.html", "_blank");
-            localStorage.setItem("lyrics", JSON.stringify(data[i]));
-        }
-
-        card.appendChild(lyricsBox);
-
-        let check = JSON.parse(localStorage.getItem('logged user'))
-        console.log(check)
-
-        if (check != null) {
-            if (check.email != "") {
-                const Favbtn = document.createElement('button');
-                Favbtn.textContent = "Add To Favorite";
-                Favbtn.classList.add('AllButtons');
-                Favbtn.onclick = function () {
-                    AddToFavorite(data[i].id,data[i].artist)
-                }
-                card.appendChild(Favbtn);
-            }
-        }
-
-        cardContainer.appendChild(card);
-    }
-
-
-
+    renderCards(data,false);
 }
 
 function errorrrenderSearchByLyrics(err) {
@@ -326,6 +211,10 @@ function errorrrenderSearchByLyrics(err) {
     cardContainer.innerHTML = "";
     swal("Something wrong", "try again", "error");
 }
+
+
+/////////-------------AddToFavorite Functions--------------////////////
+
 
 
 function AddToFavorite(idSong,NameOfArtist) {
